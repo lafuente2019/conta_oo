bc7d927bf43cfbfc406f4a43a056c5afc927e9f4
Valter Lafuente Junior <actct.vlafuente@bv.com.br>
2023-09-28 16:00:42 -0300

    Definindo de metodos para a classe conta

--- conta.py
+++ conta.py
@@ -6,4 +6,31 @@ class Conta:
         self.titular = titular
         self.saldo = saldo
         self.limite = limite
+
+    def extrato(self):
+        print(f"O saldo da conta é {self.saldo} para o titular {self.titular}") 
+
+    def depositar(self, valor):
+        self.saldo += valor
+
+    def sacar(self, valor):
+        self.saldo -= valor
+
+    def aumentar_limite(self, valor):
+        self.limite += valor
+
+    def diminuir_limite(self, valor):
+        self.limite -= valor
+
+    def cancelar_limite(self):
+        self.limite = 0       
+
+    def excluir_conta(self):
+        self.numero = None
+        self.titular = None
+        self.saldo = None
+        self.limite = None
+        print("Sua conta foi excluida com sucesso")
+
         
+
