0e27c8de0684ed556fb414938ee20417f4000f06
Valter Lafuente Junior <actct.vlafuente@bv.com.br>
2023-09-29 18:00:11 -0300

    Encapsulando os atributos

--- conta.py
+++ conta.py
@@ -2,52 +2,52 @@ class Conta:
 
     def __init__(self, numero, titular, saldo, limite):
         
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
 
     def extrato(self):
-        print(f"O saldo da conta é {self.saldo} para o titular {self.titular}") 
+        print(f"O saldo da conta é {self.__saldo} para o titular {self.__titular}") 
 
 
     def depositar(self, valor):
-        self.saldo += valor
+        self.__saldo += valor
 
     def depositar_debido_limite(self, valor):
-        self.saldo += valor    
+        self.__saldo += valor    
 
     def sacar(self, valor):
-        if self.saldo >= valor:
-            self.saldo -= valor
+        if self.__saldo >= valor:
+            self.__saldo -= valor
         else:
-            print(f"Saldo insuficiente, seu saldo é {self.saldo}")    
+            print(f"saldo insuficiente, seu saldo é {self.__saldo}")    
     
     def sacar_limite(self, valor):
-        if self.limite >= valor:
-            self.limite -= valor
+        if self.__limite >= valor:
+            self.__limite -= valor
         else:
-            print("Saldo do limite insuficiente")
+            print("saldo do limite insuficiente")
     
     def cancelar_limite(self): 
-        if self.limite < 1000:
-            valor = self.limite
+        if self.__limite < 1000:
+            valor = self.__limite
             valor -= 1000
             print("Para cancelar o limite primeiro vc precisa quitar seu debito")
             print(f"Seu debito atual é: {valor}")
         else:
-            self.limite = 0
+            self.__limite = 0
             print("Cheque especial cancelado.")    
 
     def excluir_conta(self):
-        if self.saldo > 0:
+        if self.__saldo > 0:
             print("Para cancelar sua conta primeira saque todos os valores de sua conta")
-            print(f"Saldo em conta: {self.saldo}")
+            print(f"__saldo em conta: {self.__saldo}")
         else:
-            self.numero = 0
-            self.titular = 0
-            self.saldo = 0
-            self.limite = 0
+            self.__numero = 0
+            self.__titular = 0
+            self.__saldo = 0
+            self.__limite = 0
             print("Sua conta foi excluida com sucesso")
 
         
